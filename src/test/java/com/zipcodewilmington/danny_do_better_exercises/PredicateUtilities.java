826f4946fc858b4bc5e424e9069b0f12be4998de
coreyeross25 <coreyeross12@gmail.com>
2024-07-10 01:05:28 -0400

    Finished

--- src/test/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/test/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,7 +10,7 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+        return x > y;
     }
 
     /**
@@ -19,16 +19,15 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+        return x < y;
     }
-
     /**
      * @param x
      * @param y
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+        return x >= y;
     }
 
     /**
@@ -37,7 +36,7 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+        return x <= y;
     }
 
 
@@ -45,14 +44,13 @@ public class PredicateUtilities {
      * @return true
      */
     public Boolean returnTrue() {
-        return null;
+        return true;
     }
-
     /**
      * @return false
      */
     public Boolean returnFalse() {
-        return null;
+        return false;
     }
 
 }
